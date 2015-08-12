4781aadea9b8416528cc680df39909629867b515
kujaw <kujaw@localhost.localdomain>
2015-08-12 11:17:34 +0200

    Solved SUBS excercise

--- subs.py
+++ subs.py
@@ -1,11 +1,5 @@
-#genome = 'GATATATGCATATACTT'
-#repeat = 'ATAT'
-
-f = open('rosalind_subs.txt', 'r')
-data = f.read().split('\n')
-f.close()
-genome = str(data[0])
-repeat = str(data[1])
+with open('rosalind_subs.txt', 'r') as f:
+    genome, repeat = f.read().split()
 
 repeat_indices = []
 n = 0
@@ -13,7 +7,6 @@ while len(genome[n:]) >= len(repeat):
     if repeat not in genome[n:]:
         break
     else:
-        print('Position of repeat in genome:', genome[n:].index(repeat) + n + 1)
         repeat_indices.append(genome[n:].index(repeat) + n + 1)
         n += genome[n:].index(repeat) + 1
 
